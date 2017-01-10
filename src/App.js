789831b2238acd7ded3ae3c57adb25e2cd2ea965
Giedrius Sirevicius <giedrius@icybit.co>
2017-01-10 19:33:03 +0200

    Added input binding

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'React' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Hello {this.state.name}</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <input type="text" value={this.state.name} onChange={this.handleChange} />
         </p>
       </div>
     );
