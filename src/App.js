d0f65760c5eb8a663fa4744d15a6f81bf3b8e4e7
Giedrius Sirevicius <giedrius@icybit.co>
2017-01-10 19:46:44 +0200

    Added iteration sample

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: 'React' };
+    this.state = { 
+      name: 'React',
+      TESTS: [
+        { id: 11, name: 'Test 11' },
+        { id: 12, name: 'Test 12' },
+        { id: 13, name: 'Test 13' },
+        { id: 14, name: 'Test 14' },
+        { id: 15, name: 'Test 15' },
+        { id: 16, name: 'Test 16' },
+        { id: 17, name: 'Test 17' },
+        { id: 18, name: 'Test 18' },
+        { id: 19, name: 'Test 19' },
+        { id: 20, name: 'Test 20' }
+      ]
+    };
 
     this.handleChange = this.handleChange.bind(this);
   }
@@ -24,6 +38,11 @@ class App extends Component {
         <p className="App-intro">
           <input type="text" value={this.state.name} onChange={this.handleChange} />
         </p>
+        <div>
+          <ul>
+            { this.state.TESTS.map((value) => (<li><span>{value.id}</span> {value.name}</li>)) }
+          </ul>
+        </div>
       </div>
     );
   }
